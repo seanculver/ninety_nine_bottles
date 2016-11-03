014ea38d4a4c8f37cc0c39d30d4c590c737eb7db
Sean Culver <seanculver@gmail.com>
2016-11-03 10:39:50 -0700

    Display multiple verses
    This commit spawns additional verses by calling verse again with the
    singer and new count.

--- lib/ninety_nine_bottles.ex
+++ lib/ninety_nine_bottles.ex
@@ -7,13 +7,15 @@ defmodule NinetyNineBottles do
     sing_new_verse(singer, count)
 
     receive do
-      {count} ->
+      {singer, 0} -> :noop
+      {singer, count} ->
         display_verse(count)
+        verse(singer, count - 1)
     end
   end
 
   def sing_new_verse(singer, count) do
-    spawn fn -> send(singer, { count }) end
+    spawn fn -> send(singer, {singer, count}) end
   end
 
   def display_verse(count) do
--- test/ninety_nine_bottles_test.exs
+++ test/ninety_nine_bottles_test.exs
@@ -15,10 +15,27 @@ defmodule NinetyNineBottlesTest do
     """
   end
 
-  test "Singer receives the count from a process" do
+  test "Displays multiple verses" do
+    execute_run = fn ->
+      NinetyNineBottles.sing(2)
+    end
+
+    assert capture_io(execute_run) =~
+    """
+    2 bottles of Elixir on the wall, 2 bottles of Elixir.
+    Take one down and pass it around, 1 bottles of Elixir on the wall.
+
+    1 bottles of Elixir on the wall, 1 bottles of Elixir.
+    Take one down and pass it around, 0 bottles of Elixir on the wall.
+    """
+  end
+
+  test "Singer receives the count and singer from a process" do
     singer = self()
     NinetyNineBottles.sing_new_verse(singer, 1)
 
-    assert_receive {1}
+    assert_receive {singer, 1}
   end
+
+
 end
