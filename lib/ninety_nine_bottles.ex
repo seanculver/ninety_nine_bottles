b498618773c0ac772410940168d48342eb6d554c
Sean Culver <seanculver@gmail.com>
2016-12-01 06:53:12 -0800

    Add different pluralizations

--- lib/ninety_nine_bottles.ex
+++ lib/ninety_nine_bottles.ex
@@ -7,22 +7,40 @@ defmodule NinetyNineBottles do
     sing_new_verse(singer, count)
 
     receive do
-      {singer, 0} -> :noop
-      {singer, count} ->
-        display_verse(count)
+      {singer, new_verse, 0} ->
+        display_end
+      {singer, new_verse, count} ->
+        display_verse(new_verse, count)
         verse(singer, count - 1)
     end
   end
 
   def sing_new_verse(singer, count) do
-    spawn fn -> send(singer, {singer, count}) end
+    spawn fn -> send(singer, {singer, self(), count}) end
   end
 
-  def display_verse(count) do
-    """
-    #{count} bottles of Elixir on the wall, #{count} bottles of Elixir.
+  def display_end do
+    IO.puts "Go code some more, 99 bottles of Elixir on the wall."
+  end
+
+  def display_verse(new_verse, count) when count == 1 do
+    IO.puts """
+    #{inspect new_verse} #{count} bottle of Elixir on the wall, #{count} bottle of Elixir.
     Take one down and pass it around, #{count - 1} bottles of Elixir on the wall.
     """
-      |> IO.puts
+  end
+
+  def display_verse(new_verse, count) when count == 2 do
+    IO.puts """
+    #{inspect new_verse} #{count} bottles of Elixir on the wall, #{count} bottles of Elixir.
+    Take one down and pass it around, #{count - 1} bottle of Elixir on the wall.
+    """
+  end
+
+  def display_verse(new_verse, count) do
+    IO.puts """
+    #{inspect new_verse} #{count} bottles of Elixir on the wall, #{count} bottles of Elixir.
+    Take one down and pass it around, #{count - 1} bottles of Elixir on the wall.
+    """
   end
 end
--- test/ninety_nine_bottles_test.exs
+++ test/ninety_nine_bottles_test.exs
@@ -1,41 +1,57 @@
 defmodule NinetyNineBottlesTest do
   use ExUnit.Case
+  require Logger
 
   import ExUnit.CaptureIO
 
-  test "Displays a single verse" do
-    execute_run = fn ->
-      NinetyNineBottles.sing(1)
-    end
-
-    assert capture_io(execute_run) =~
-    """
-    1 bottles of Elixir on the wall, 1 bottles of Elixir.
-    Take one down and pass it around, 0 bottles of Elixir on the wall.
-    """
-  end
-
-  test "Displays multiple verses" do
-    execute_run = fn ->
-      NinetyNineBottles.sing(2)
-    end
-
-    assert capture_io(execute_run) =~
-    """
-    2 bottles of Elixir on the wall, 2 bottles of Elixir.
-    Take one down and pass it around, 1 bottles of Elixir on the wall.
-
-    1 bottles of Elixir on the wall, 1 bottles of Elixir.
-    Take one down and pass it around, 0 bottles of Elixir on the wall.
-    """
-  end
-
-  test "Singer receives the count and singer from a process" do
+  test "Singer receives the singer pid, new pid, and count from a process" do
     singer = self()
     NinetyNineBottles.sing_new_verse(singer, 1)
 
-    assert_receive {singer, 1}
+    assert_receive {pid1, pid2, 1}
+    assert pid1 == singer
+    refute pid1 == pid2
   end
 
+  test "Displays a singular end verse" do
+    verse_pid = self()
+    execute_run = fn ->
+      NinetyNineBottles.display_verse(verse_pid, 1)
+    end
 
+    assert capture_io(execute_run) ==
+    """
+    #{inspect verse_pid} 1 bottle of Elixir on the wall, 1 bottle of Elixir.
+    Take one down and pass it around, 0 bottles of Elixir on the wall.
+
+    """
+  end
+
+  test "Displays a singular verse" do
+    verse_pid = self()
+    execute_run = fn ->
+      NinetyNineBottles.display_verse(verse_pid, 2)
+    end
+
+    assert capture_io(execute_run) ==
+    """
+    #{inspect verse_pid} 2 bottles of Elixir on the wall, 2 bottles of Elixir.
+    Take one down and pass it around, 1 bottle of Elixir on the wall.
+
+    """
+  end
+
+  test "Displays a plural verse" do
+    verse_pid = self()
+    execute_run = fn ->
+      NinetyNineBottles.display_verse(verse_pid, 3)
+    end
+
+    assert capture_io(execute_run) ==
+    """
+    #{inspect verse_pid} 3 bottles of Elixir on the wall, 3 bottles of Elixir.
+    Take one down and pass it around, 2 bottles of Elixir on the wall.
+
+    """
+  end
 end
